e1596615ecd56d6bbfe018ac787de389a736f8ea
Kaushik Jain <kaushikjain67890@gmail.com>
2022-01-10 09:08:43 +0530

    Matrix Revise

--- 2-D ARRAY/multiply.cpp
+++ 2-D ARRAY/multiply.cpp
@@ -1,51 +1,56 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
- 
+
 int main()
-{   int r1,r2,c1,c2;
+{
+    int r1, r2, c1, c2;
     int arr[15][15];
     int arr2[15][15];
-    int result [15][15];
+    int result[15][15];
 
-     cout << "Enter rows and columns for first matrix: ";
+    cout << "Enter rows and columns for first matrix: ";
     cin >> r1 >> c1;
     cout << "Enter rows and columns for second matrix: ";
     cin >> r2 >> c2;
 
-    cout<<"Enter elements of 1st array "<<endl;
+    cout << "Enter elements of 1st array " << endl;
     for (int i = 0; i < r1; i++)
     {
         for (int j = 0; j < c1; j++)
         {
-           cin>>arr[i][j];
+            cout << "Enter element at " << i << j << " : ";
+            cin >> arr[i][j];
         }
-        cout<<endl;
+        cout << endl;
     }
-    cout<<"Enter elements of 2nd array "<<endl;
+    cout << "Enter elements of 2nd array " << endl;
     for (int i = 0; i < r2; i++)
     {
         for (int j = 0; j < c2; j++)
         {
-           cin>>arr2[i][j];
+            cout << "Enter element at " << i << j << " : ";
+            cin >> arr2[i][j];
         }
-        cout<<endl;
+        cout << endl;
     }
-        cout<<"\nMultiplication of these two array are  "<<endl;
+    cout << "\nMultiplication of these two array are  " << endl;
 
-  for (int i = 0; i < r1; i++)
+    // for (int i = 0; i < r2; i++)
+    // {
+    //     for (int j = 0; j < c2; j++)
+    //     {
+    //         result[i][j] = 0;
+    //     }
+    // }
+    for (int i = 0; i < r1; i++) // result matrix ki row k liyae
     {
-        for (int j = 0; j < c2; j++)
+        for (int j = 0; j < c2; j++) // result matrix ki column k liyae
         {
-            result[i][j]=0;
-        }}
-    for (int i = 0; i < r1; i++)
-    {
-        for (int j = 0; j < c2; j++)
-        {
-            for(int k = 0; k < c1; ++k)
+            result[i][j] = 0;
+            for (int k = 0; k < c1; k++)
             {
-            result[i][j]+=arr[i][k]*arr2[k][j];
+                result[i][j] += arr[i][k] * arr2[k][j];
             }
         }
     }
@@ -54,10 +59,17 @@ int main()
     {
         for (int j = 0; j < c2; j++)
         {
-         cout<<result[i][j]<<" ";
-        }cout<<"\n";
-        // cout<<"\n";
-    }
-
-   
+            cout << result[i][j] << " ";
+        }
+        cout << "\n";
+        }
 }
+
+
+// Input
+// 2 2
+// 2 3
+// 1 2
+// 4 1
+// 4 5 6
+// 5 9 1
--- 2-D ARRAY/pointers.cpp
+++ 2-D ARRAY/pointers.cpp
@@ -6,7 +6,7 @@ int main()
 {
     // address of arr[0]=1 is 0x61fee8
     // address of arr[1]+1=5 is 0x61fef8
-    int a[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int a[3][3] = {1, 2, 3, 4 ,5, 6, 7, 8, 9};
     int *p;
     p = a[0];
     //p = &arr[0]; //Error = a value of type "int (*)[3]" cannot be assigned to an entity of type "int *"
